0d46cf321299147181a5a3c1fe8657ef456202ee
marqueskennedy-netizen <marques.kennedy@escola.pr.gov.br>
2026-04-14 09:06:44 -0300

    Add files via upload

---
+++ lista-de-exercicio.js
@@ -0,0 +1,41 @@
+//adades com for 
+
+//1: exiba os numro de 1 a 10 uando for
+
+for(let i=0; i<=10; i++){
+    console.log(i);
+
+
+
+}
+//2. exiba os numeros de 10 a 1 em ordem decrscente
+for(let i=10; i>=1; i--){
+    console.log(i)
+}
+//3.exiba todos os numeros pares e 2 a 20
+for(let i=2; i<=20; i= i+2){
+    console.log(i);
+}
+
+
+//4. Exiba a tabuada o numero 5(5x1 ate 5x10)
+for(let i=1; i<=10; i++){
+    console.log(`5 x ${i}=${5*i}`);
+}
+
+
+//5.calcule e exiba a soma do numeros e 1 ate 50
+for(let i=1; i<=50; i++){
+    soma = soma + i;}
+    console.log(`A soma é: ${soma}`);
+
+
+//6.conte de 1 ate o numero 8 (use o for)
+for(let i=1; i<=8; i++){
+    console.log(i)
+}
+
+//7.Exiba todos o numeros impare de 1 a 15
+for(let i= 1; i<=15; i= i+2){
+    console.log(i);
+}
